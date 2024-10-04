1ce27b2c8cd98b0fb9eb42c71c55f167d9164a04
Lucas M. Campos <lucasof.net@gmail.com>
2024-10-04 20:53:53 -0300

    Code refactoring

--- blackjack.js
+++ blackjack.js
@@ -12,12 +12,15 @@ var hiddenCard;
 var deck = [];
 var canHit = true;
 var canStay = true;
+var firstTime = true;
 
 window.onload = function()
 {
     document.getElementById("hit-btn").addEventListener("click", hit);
     document.getElementById("stay-btn").addEventListener("click", stay);
     document.getElementById("play-again-btn").addEventListener("click", playAgain);
+    document.getElementById("play-again-btn").style.visibility = "hidden";
+
 
     buildDeck();
     shuffleDeck();
@@ -54,24 +57,27 @@ function shuffleDeck()
 
 }
 
-function startGame()
+async function startGame()
 {
+    let ms = firstTime ? 0 : 500;
+
     hand.set("dealer", 0);
     hand.set("player", 0);
     aceCount.set("dealer", 0);
     aceCount.set("player", 0);
 
     addHiddenCard();
+    await wait(ms);
     addCardTo("dealer");
+    await wait(ms);
 
     addCardTo("player");
+    await wait(ms);
     addCardTo("player");
-
+    
     canHit = true;
     canStay = true;
-    
-    document.getElementById("play-again-btn").style.visibility = "hidden";
-
+    firstTime = false;
 }
 
 async function hit()
@@ -86,7 +92,7 @@ async function hit()
     if(getHand("player") > 21)
     {
         await wait(500);
-        stay();
+        await stay();
     }
 }
 
@@ -95,7 +101,7 @@ function getHand(subject)
     while(hand.get(subject) > 21 && aceCount.get(subject) > 0)
     {
         addValueToHand(-10, subject);
-        unregisterAceTo(subject);
+        adjustAceCount(-1, subject);
     }
 
     return hand.get(subject);
@@ -130,7 +136,13 @@ function addHiddenCard()
     hiddenCard = document.createElement("img");
     hiddenCard.src = "assets/cards/hidden.png";
 
-    document.getElementById("dealer-hand").appendChild(hiddenCard);
+    spawnCard(hiddenCard, "dealer");
+}
+
+function spawnCard(card, subject)
+{
+    cardSfx.play();
+    document.getElementById(subject+"-hand").appendChild(card);
 }
 
 function revealCard()
@@ -139,7 +151,6 @@ function revealCard()
     hiddenCard.src = createCard(card).src;
 
     addValueToHand(getCardValue(card), "dealer");
-
     cardSfx.play();
 }
 
@@ -149,48 +160,40 @@ function checkWinner()
     let dealer = getHand("dealer");
     let player = getHand("player");
 
-    if(player > 21)
-    {
+    if (player > 21) {
         status.innerText = "Dealer won!\nPlayer busted";
-    }
-    else if(dealer > 21)
-    {
+    } else if (dealer > 21) {
         status.innerText = "Player won!\nDealer busted";
-    }
-    else if(player > dealer)
-    {
-        status.innerText = "Player won!";
-    }
-    else if (dealer > player)
-    {
-        status.innerText = "Dealer won!";
-    }
-    else
-    {
+    } else if (player === dealer) {
         status.innerText = "Draw!";
+    } else {
+        status.innerText = player > dealer ? "Player won!" : "Dealer won!";
     }
 
     gameOverSfx.play();
-
-    document.getElementById("play-again-btn").style.visibility = "visible";
-    document.getElementById("hit-btn").style.visibility = "hidden";
-    document.getElementById("stay-btn").style.visibility = "hidden";
+    endGame();
 }
 
-function playAgain()
-{
+function endGame() {
+    document.getElementById("play-again-btn").style.visibility = "visible";
+    document.getElementById("play-again-btn").focus(); 
+    document.getElementById("hit-btn").style.visibility = "hidden";
+    document.getElementById("stay-btn").style.visibility = "hidden";
+    
+}
+
+
+function clearHands() {
+    document.getElementById("dealer-hand").innerHTML = '';
+    document.getElementById("player-hand").innerHTML = '';
+}
+
+function playAgain() {
+    document.getElementById("play-again-btn").style.visibility = "hidden";
     document.getElementById("hit-btn").style.visibility = "visible";
     document.getElementById("stay-btn").style.visibility = "visible";
-
-    let dealer = document.getElementById("dealer-hand");
-    let player = document.getElementById("player-hand");
-
-    while (dealer.hasChildNodes())
-        dealer.firstChild.remove()
-    
-    while (player.hasChildNodes())
-        player.firstChild.remove()
-
+    document.getElementById("game-status").innerText = "";
+    clearHands();
     startGame();
 }
 
@@ -200,9 +203,7 @@ function addCardTo(subject)
     let value = getCardValue(card);
     
     addValueToHand(value, subject);
-
-    cardSfx.play();
-    document.getElementById(subject+"-hand").appendChild(createCard(card));
+    spawnCard(createCard(card), subject);
 }
 
 function createCard(card)
@@ -225,18 +226,13 @@ function addValueToHand(value, subject)
 
     if(value == 11)
     {
-        registerAceTo(subject);
+        adjustAceCount(1, subject);
     }
 }
 
-function registerAceTo(subject)
+function adjustAceCount(increment, subject)
 {
-    aceCount.set(subject, aceCount.get(subject) + 1);
-}
-
-function unregisterAceTo(subject)
-{
-    aceCount.set(subject, aceCount.get(subject) - 1);
+    aceCount.set(subject, aceCount.get(subject) + increment);
 }
 
 function getCardValue(card)
